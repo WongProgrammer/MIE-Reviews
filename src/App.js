dd60be59ab9de3bbb9633fd4df9a5d0bb6d2f39a
WongProgrammer <francisnguyen2344@gmail.com>
2022-01-26 19:55:49 -0800

    stars and styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import { Container, Typography } from '@mui/material';
+import { Box } from '@mui/system';
 import Reviews from './Reviews';
 function App() {
   
   return (
-    <Container>
-      <Typography variant='h1'>Hello</Typography>
+    <Container > 
+      <Box
+      sx={{
+        backgroundImage: 'linear-gradient(#e66465, #DAF7A6)',
+        borderRadius:'5px',
+        marginTop: '2%',
+        marginBottom: '5%'
+      }}>
+      <Typography align='center' variant='h1' sx={{color:'#181818'}}>MIE Reviews</Typography>
+      </Box>
+      
       <Reviews/>
     </Container>
   );
--- src/Reviews.js
+++ src/Reviews.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useState } from 'react';
 import reviews from './data'
-import { Box, Button, Card, CardContent, CardActionArea, IconButton, Typography, CardMedia } from '@mui/material'
+import { Box, Button, Card, CardContent, IconButton, Typography, CardMedia, Rating } from '@mui/material'
 import { NavigateNext, NavigateBefore } from '@mui/icons-material'
 function Reviews() {
     const [index, setIndex] = useState(0);
@@ -19,37 +19,52 @@ function Reviews() {
 
     const randomIndex = () => {
         let newIndex = Math.floor(Math.random() * reviews.length);
-        while(newIndex === index) {
+        while (newIndex === index) {
             newIndex = Math.floor(Math.random() * reviews.length);
         }
         setIndex(newIndex);
     }
 
     return (
-        <Card >
-             <CardMedia
+        <Card sx={{ display: 'flex', backgroundColor: '#DAF7A6' }}>
+            <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+            <CardMedia
                 component="img"
                 image={image}
                 sx={{
-                    height: 350,
-                    width: 350,
-                    borderRadius : '50%'
+                    height: 250,
+                    width: 250,
+                    borderRadius: '50%',
+                    margin: '10px',
+                    border: 'solid'
+                    
                 }}
                 alt={`${image}'s profile picture`}
             />
+            <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+            <IconButton color='secondary' onClick={prevIndex}><NavigateBefore /></IconButton>
+                    <IconButton color='secondary' onClick={nextIndex}><NavigateNext /></IconButton>
+
+            </Box>
+                <Button color='secondary' onClick={randomIndex}><b>Random</b></Button>
+            </Box>
+          
             <Box >
                 <CardContent >
-                    <Typography component="div">{name}</Typography>
+                    <Typography component="div" variant='h3'>{name}</Typography>
+                    <Rating name="half-rating-read" value={stars} precision={0.5} readOnly />
+                    <Typography variant='subtitle1' ><b>Time used:</b> {time_used}</Typography>
+                    <Typography variant='subtitle1' ><b>Company size:</b> {size}</Typography>
                 </CardContent>
-            
-                <CardActionArea>
-                    <IconButton onClick={prevIndex}><NavigateBefore /></IconButton>
-                    <IconButton onClick={nextIndex}><NavigateNext /></IconButton>
-                   
-                </CardActionArea>
-                <Button onClick={randomIndex}>Random</Button>
+                <CardContent>
+                    <Typography variant='body1'><b>Pros:</b> {pros}</Typography>
+                    <Typography variant='body1'><b>Cons:</b> {cons}</Typography>
+                </CardContent>
+                
             </Box>
            
+                
+            
         </Card>
     );
 }
